44c31e0b1b548cc4f5551d3e661253442417111c
Leckie <laixianbo@bbdservice.com>
2019-07-09 16:11:46 +0800

    feat: spring bus & mq

--- jka-client/src/main/java/me/leckie/jka/client/web/controller/ConfigController.java
+++ jka-client/src/main/java/me/leckie/jka/client/web/controller/ConfigController.java
@@ -5,6 +5,7 @@ import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping
+@RefreshScope
 public class ConfigController {
 
   private Logger logger = LoggerFactory.getLogger(ConfigController.class);
--- jka-feign/src/main/java/me/leckie/jka/feign/web/controller/HiFeignController.java
+++ jka-feign/src/main/java/me/leckie/jka/feign/web/controller/HiFeignController.java
@@ -2,7 +2,9 @@ package me.leckie.jka.feign.web.controller;
 
 import me.leckie.jka.feign.service.HiFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,11 @@ public class HiFeignController {
   @GetMapping("/port")
   public String getPort() {
     return hiFeignService.valueOfFieldName("port");
+  }
+
+  @GetMapping("{fieldName}")
+  public String getValueOfField(@PathVariable String fieldName) {
+    return hiFeignService.valueOfFieldName(fieldName);
   }
 
 }
